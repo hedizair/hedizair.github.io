40545d3afa73692c524160a9eb2413c9370ef300
hedizair <hedi.marwan@hotmail.fr>
2026-05-23 20:13:35 +0200

    fix vit econfig

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // ✅ '/' car c'est le repo principal tonpseudo.github.io
   plugins: [
     vue(),
     vueDevTools(),
